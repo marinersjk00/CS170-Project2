a8193c8e6eb3230fb8faa09c3130b403fed7d72e
Jordan K <kuschnerj@gmail.com>
2022-12-08 08:42:07 -0800

    Add files via upload

--- jordanKuschnerCS170.m
+++ jordanKuschnerCS170.m
@@ -1,14 +1,14 @@
 function maxAcc = jkProject2()
-    data = load("/MATLAB Drive/Projects/Feature Selection/CS170_Small_Data__96.txt");
+    data = load("/MATLAB Drive/Projects/Feature Selection/CS170_Large_Data__96.txt");
     
     maxAcc = 0;
     featureSet = [];
-    ind = 1;
+
     tic
-    feature_search(data);
+    feature_search_forwards(data);
     toc
     disp(featureSet);
-    function currentSet = feature_search(data)
+    function currentSet = feature_search_forwards(data)
     
     
     currentSet = [];
@@ -38,16 +38,56 @@ function maxAcc = jkProject2()
             disp(currentSet)
             if bestAccSoFar > maxAcc
                             maxAcc = bestAccSoFar;
-                            featureSet = [];
-                            featureSet = currentSet
+                            % featureSet = [];
+                            featureSet = currentSet;
+                            %ind = ind + 1;
             end
            
         end
     end
+
+ function currentSet = feature_search_backwards(data)
+    
+    
+    currentSet = [];
+        for i = 1 : size(data,2)-1
+            disp(["On the ", int2str(i), "th level of the search tree"]);
+            featureToAdd = [];
+            bestAccSoFar = 0;
+          
+            for k = 1 : size(data, 2)-1
+                 if isempty(intersect(currentSet, k))
+                     disp(["-->Considering adding ", int2str(k), "th feature..."]);
+                 
+                     accuracy = leaveOneOut(data, currentSet, k) 
+        
+                    if accuracy > bestAccSoFar
+                        bestAccSoFar = accuracy;
+                        featureToAdd = k;
+                      
+                    end
+                    
+                
+                end
+            end
+
+            currentSet(i) = featureToAdd;
+            disp(["On level ", num2str(i), " I added feature ", num2str(featureToAdd), " to the set." ])
+            disp(currentSet)
+            if bestAccSoFar > maxAcc
+                            maxAcc = bestAccSoFar;
+                            % featureSet = [];
+                            featureSet = currentSet;
+                            %ind = ind + 1;
+            end
+           
+        end
+ end
+
     
     function accuracy = leaveOneOut(data, currentSet, featureToAdd)
     for i = 2 : size(data, 2)
-        if isempty(intersect(currentSet, i-1)) && featureToAdd ~= (i-1)
+        if isempty(intersect(currentSet, i-1)) && i-1 ~= featureToAdd
                data(:, i) = 0;
         end
     end
@@ -62,7 +102,7 @@ function maxAcc = jkProject2()
         for k = 1 : size(data, 1)
             
             if k ~= i
-                disp(['Is ', int2str(i), ' a nearest neighbor with ', int2str(k), '?']);
+                %disp(['Is ', int2str(i), ' a nearest neighbor with ', int2str(k), '?']);
                 distance = sqrt(sum((testObj - data(k, 2:end)).^2));
                 if distance < nearestNeighborDist
                     nearestNeighborDist = distance;
