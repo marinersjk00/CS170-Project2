fe9c9f3f44150047ff88f0951a9a06a644cf1c69
Jordan K <kuschnerj@gmail.com>
2022-12-07 19:17:05 -0800

    Add files via upload

--- jordanKuschnerCS170.m
+++ jordanKuschnerCS170.m
@@ -1,31 +1,35 @@
 
-function feature_search(data)
+function feature_search()
 data = load("/MATLAB Drive/Projects/Feature Selection/CS170_Small_Data__96.txt");
 
 currentSet = [];
-
-for i = 1 : size(data,1)
-    disp(["On the ", int2str(i), "th level of the search tree"]);
-    featureToAdd = [];
-    bestAccSoFar = 0;
-     for k = 1 : size(data, 2)-1;
-         if isempty(intersect(currentSet, k))
-             disp(["-->Considering adding ", int2str(k), "th feature..."]);
-             accuracy = leaveOneOut(data, currentSet, k + 1) 
-
-            if accuracy > bestAccSoFar
-                bestAccSoFar = accuracy;
-                featureToAdd = k;
+    for i = 1 : size(data,2)-1
+        disp(["On the ", int2str(i), "th level of the search tree"]);
+        featureToAdd = [];
+        bestAccSoFar = 0;
+        for k = 1 : size(data, 2)-1
+             if isempty(intersect(currentSet, k))
+                 disp(["-->Considering adding ", int2str(k), "th feature..."]);
+                 accuracy = leaveOneOut(data, currentSet, k + 1) 
+    
+                if accuracy > bestAccSoFar
+                    bestAccSoFar = accuracy;
+                    featureToAdd = k;
+                end
             end
         end
+
+        currentSet(i) = featureToAdd;
+        disp(["On level ", num2str(i), " I added feature ", num2str(featureToAdd), " to the set." ])
     end
-    currentSet(i) = featureToAdd;
-    disp(["On level ", num2str(i), " I added feature ", num2str(featureToAdd), " to the set." ])
-end
 end
 
 function accuracy = leaveOneOut(data, currentSet, featureToAdd)
-
+for i = 1 : size(data, 1)
+    if isempty(intersect(currentSet, i))
+           data(:, i) = 0;
+    end
+end
 numCorrect = 0;
 for i = 1 : size(data, 1)
     testObj = data(i, 2:end);
@@ -50,8 +54,8 @@ for i = 1 : size(data, 1)
         numCorrect = numCorrect + 1;
 
     end
-    accuracy = numCorrect / size(data, 1);
 
 end
+    accuracy = numCorrect / size(data, 1);
 end
 
