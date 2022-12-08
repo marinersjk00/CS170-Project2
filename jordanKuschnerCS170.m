6f9ef88a1a8ed3fcbc07d196b025057298fcbc05
Jordan K <kuschnerj@gmail.com>
2022-12-07 18:30:43 -0800

    Add files via upload

--- jordanKuschnerCS170.m
+++ jordanKuschnerCS170.m
@@ -1,7 +1,32 @@
-function accuracy = jordanKuschnerCS170()
-data = load("/MATLAB Drive/Projects/Feature Selection/CS170_Small_Data__96.txt");
-numCorrect = 0;
 
+function feature_search(data)
+data = load("/MATLAB Drive/Projects/Feature Selection/CS170_Small_Data__96.txt");
+
+currentSet = [];
+
+for i = 1 : size(data,1)
+    disp(["On the ", int2str(i), "th level of the search tree"]);
+    featureToAdd = [];
+    bestAccSoFar = 0;
+     for k = 1 : size(data, 2)-1;
+         if isempty(intersect(currentSet, k))
+             disp(["-->Considering adding ", int2str(k), "th feature..."]);
+             accuracy = leaveOneOut(data, currentSet, k + 1) 
+
+            if accuracy > bestAccSoFar
+                bestAccSoFar = accuracy;
+                featureToAdd = k;
+            end
+        end
+    end
+    currentSet(i) = featureToAdd;
+    disp(["On level ", num2str(i), " I added feature ", num2str(featureToAdd), " to the set." ])
+end
+end
+
+function accuracy = leaveOneOut(data, currentSet, featureToAdd)
+
+numCorrect = 0;
 for i = 1 : size(data, 1)
     testObj = data(i, 2:end);
     testLabel = data(i, 1);
@@ -27,5 +52,6 @@ for i = 1 : size(data, 1)
     end
     accuracy = numCorrect / size(data, 1);
 
+end
 end
 
